32c0e2e5a667fc6297564c8fa58d0378907d0c6e
Matthew Harrold <github@marrold.co.uk>
2024-04-09 15:37:41 +0100

    Bump node version

--- lambda.tf
+++ lambda.tf
@@ -71,7 +71,7 @@ resource "aws_lambda_function" "lambda" {
 
   publish = true
   handler = "index.handler"
-  runtime = "nodejs20.x"
+  runtime = "nodejs16.x"
   role    = aws_iam_role.lambda_at_edge.arn
 
 }
