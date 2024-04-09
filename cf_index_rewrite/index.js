822ee67cb1672869adbce7e9c562d61f9c7039fa
Matthew Harrold <github@marrold.co.uk>
2024-04-09 15:30:00 +0100

    update re-write lambda

--- cf_index_rewrite/index.js
+++ cf_index_rewrite/index.js
@@ -7,17 +7,22 @@ exports.handler = (event, context, callback) => {
     // Extract the URI from the request
     var olduri = request.uri;
 
-    // Match any '/' that occurs at the end of a URI. Replace it with a default index
-    var newuri = olduri.replace(/\/$/, '\/index.html');
-    
+    // Regular expression to match a filename with a 3 or 4 character extension
+    var filePattern = /\/[^\/]+\.[a-zA-Z0-9]{3,4}$/;
+
+    // Check if the URI does not end with a filename format 'something.format'
+    if (!filePattern.test(olduri)) {
+        // If not, replace it with a default index
+        olduri = olduri.replace(/\/?$/, '/index.html');
+    }
+
     // Log the URI as received by CloudFront and the new URI to be used to fetch from origin
     console.log("Old URI: " + olduri);
-    console.log("New URI: " + newuri);
+    console.log("New URI: " + olduri); // Note: The variable should be 'olduri' since it's updated now
     
-    // Replace the received URI with the URI that includes the index page
-    request.uri = newuri;
+    // Replace the received URI with the updated URI
+    request.uri = olduri;
     
     // Return to CloudFront
     return callback(null, request);
-
 };
